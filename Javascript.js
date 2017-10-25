6a300c598c4d9367ad0e8872ab3c3f1f80de2be4
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-25 10:30:12 +0200

    Exercice 8 : Javascript

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+function form1(){
+  var age = document.getElementById("age").value;
+  if(age >= 18){
+    alert("T'es qu'un gros pédophile de merde !");
+  }
+  else{
+    alert("Va crever sur le bord d'une autoroute !");
+  }
+}
