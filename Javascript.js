8f79ba19f6a924116a5aa6deca98f9c3b9308ef9
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-26 09:37:04 +0200

    Correction

--- Javascript.js
+++ Javascript.js
@@ -1,9 +1,13 @@
 function form1(){
   var age = document.getElementById("age").value;
+if(isNaN(age) == false && age > 0){
   if(age >= 18){
     alert("T'es majeur, mec !");
   }
   else{
     alert("T'es encore un gamin, fiston !");
+  }}
+  else{
+    alert("Met ton âge !")
   }
 }
